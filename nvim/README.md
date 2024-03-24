c9918b0e4e56815f4483bdf5181defcb380ee7fd
Damian <dzoltow@gmail.com>
2024-03-24 01:54:51 +0100

    add initial nvim config

---
+++ nvim/README.md
@@ -0,0 +1,6 @@
+# 💤 LazyVim
+
+## Requirements
+
+- [LazyGit](https://github.com/jesseduffield/lazygit)
+- [RipGrep](https://github.com/BurntSushi/ripgrep)
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,41 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local util = require("util.cowboy")
+
+util.cowboy()
+
+local builtin = require("telescope.builtin")
+local keymap = vim.keymap
+
+-- split window
+keymap.set("n", "<leader>w\\", "<C-W>v", { desc = "Split window right", remap = true })
+
+-- telescope
+keymap.set("n", "<leader>fw", function()
+  builtin.grep_string({ search = vim.fn.input("Grep For > ") })
+end, { desc = "Fuzzy find words" })
+
+-- keep line join
+keymap.set("n", "<leader>j", "J")
+
+-- center view on search next/previous word
+keymap.set("n", "n", "nzz")
+keymap.set("n", "N", "Nzz")
+
+-- copy to and paste from system clipboard
+keymap.set("n", "<leader>cp", '"+p')
+keymap.set("n", "<leader>cy", '"+y')
+
+-- TODO
+-- paste over other highlighted block without loosing current p buffer
+keymap.set("x", "<leader>p", [["_dP]])
+
+-- delete without saving to the buffer
+keymap.set({ "n", "v" }, "<leader>d", '"_d')
+
+-- replace what is under the cursor case insensitive in the whole file
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
